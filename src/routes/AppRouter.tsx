7095bc042e5d787a1e9b3d1bf378dc97473256af
agudelotomas02 <agudelotomas02@gmail.com>
2025-05-18 21:21:31 -0500

    corrección de ruta principal

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from '../pages/Login';
 import Inventario from '../pages/Inventario';
 import PosRestaurantes from '../pages/PosRestaurantes';
 import Update from '../pages/Update';
 import Orders from '../pages/Orders';
-import RestaurantesCliente from '../pages/RestaurantesCliente'; // <- este es nuevo
+import RestaurantesCliente from '../pages/RestaurantesCliente';
 import ProductosCliente from '../pages/ProductosCliente';
-
 
 const AppRouter = () => (
   <BrowserRouter>
     <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/login" element={<Login />} />
       <Route path="/cliente/restaurantes" element={<RestaurantesCliente />} />
       <Route path="/pos/restaurantes" element={<PosRestaurantes />} />
@@ -18,7 +18,6 @@ const AppRouter = () => (
       <Route path="/pos/:restaurante/update" element={<Update />} />
       <Route path="/pos/:restaurante/orders" element={<Orders />} />
       <Route path="/cliente/restaurantes/:restauranteId/productos" element={<ProductosCliente />} />
-
     </Routes>
   </BrowserRouter>
 );
